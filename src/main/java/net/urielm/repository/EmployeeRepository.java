45a45291a7cf030d8955de0ea9d7e2e7f9858e18
urielsmp128 <urielsmp128@outlook.com>
2024-05-28 22:17:16 -0600

    Employee Repository created

---
+++ src/main/java/net/urielm/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package net.urielm.repository;
+
+import net.urielm.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
