709499a8180b640e74454edf3c48ae76fc97aed8
urielsmp128 <urielsmp128@outlook.com>
2024-06-07 14:39:46 -0600

    Implementing Department Entity and Repository

---
+++ src/main/java/net/urielm/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package net.urielm.repository;
+
+import net.urielm.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
